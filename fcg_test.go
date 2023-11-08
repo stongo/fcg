9b408e05a97248b9f729e4e61e02f6b2a3d0b0f1
Marcus Stong <stongo@gmail.com>
2023-11-08 22:41:48 +0000

    tests most methods and functions

--- fcg_test.go
+++ fcg_test.go
@@ -1,86 +1,185 @@
-package main
+package fcg
 
 import (
-  "testing"
-  "fmt"
+	"fmt"
+	"testing"
 )
 
 func errorMsg(message string, expect, actual interface{}) string {
-    return fmt.Sprintf("%s. Got: %#v Expected: %#v", message, actual, expect)
+	return fmt.Sprintf("%s. Got: %#v Expected: %#v", message, actual, expect)
 }
 
-func TestNewBuilder(t *testing.T) {
-  expect := &Builder{}
-  actual := NewBuilder()
-  if len(actual.graph) != len(expect.graph) {
-    t.Error(errorMsg("Values don't match.", actual, expect))
-  }
+func TestBuilderNewBuilder(t *testing.T) {
+	expect := &Builder{}
+	actual := NewBuilder()
+	if len(actual.graph) != len(expect.graph) {
+		t.Error(errorMsg("Values don't match.", actual, expect))
+	}
 }
 
-func TestAddNode(t *testing.T) {
-  b := NewBuilder() 
-  var v uint64
-  expect := &Node{}
-  actual := b.AddNode(v)
-  if actual.value != expect.value {
-    t.Error(errorMsg("Values don't match.", actual, expect))
-  }
-  actual.value = 1
-  if actual.value != b.graph[0].value {
-    t.Error(errorMsg("Values don't match.", actual, expect)) 
-  }
-  if actual.value == expect.value {
-    t.Error(errorMsg("Values shouldn't match.", actual, expect)) 
-  }
-  b.graph[0].value = 2
-  if actual.value != 2 {
-    t.Error(errorMsg("Value should be updated.", actual, expect)) 
-  }
+func TestBuilderAddNode(t *testing.T) {
+	b := NewBuilder()
+	var v uint64
+	expect := &Node{}
+	actual := b.AddNode(v)
+	if actual.value != expect.value {
+		t.Error(errorMsg("Values don't match.", actual, expect))
+	}
+	actual.value = 1
+	if actual.value != b.graph[0].value {
+		t.Error(errorMsg("Values don't match.", actual, expect))
+	}
+	if actual.value == expect.value {
+		t.Error(errorMsg("Values shouldn't match.", actual, expect))
+	}
+	b.graph[0].value = 2
+	if actual.value != 2 {
+		t.Error(errorMsg("Value should be updated.", actual, expect))
+	}
 }
 
-func TestInit(t *testing.T) {
-  b := NewBuilder()
-  expect := &Node{
-    value: 3,
-  }
-  actual := b.Init(3)
-  if actual.value != expect.value {
-    t.Error(errorMsg("unexpected value", actual.value, expect.value)) 
-  } 
+func TestBuilderInit(t *testing.T) {
+	b := NewBuilder()
+	expect := &Node{
+		value: 3,
+	}
+	actual := b.Init(3)
+	if actual.value != expect.value {
+		t.Error(errorMsg("unexpected value", actual.value, expect.value))
+	}
 }
 
-func TestAddEdge(t *testing.T) {
-  b := NewBuilder()
-  var v uint64
-  expect := make(map[string]*Node)
-  expect["Node0"] = b.AddNode(v)
-  expect["Node1"] = b.AddNode(v)
-  expect["Node2"] = b.AddNode(v)
-  b.addEdge(*expect["Node0"], *expect["Node1"], ADD)
-  b.addEdge(*expect["Node1"], *expect["Node2"], MULT)
+func TestBuilderAddEdge(t *testing.T) {
+	b := NewBuilder()
+	var v uint64
+	expect := make(map[string]Edge)
+	expect["edge0"] = Edge{
+		keys:     []int{0, 1},
+		operator: ADD,
+	}
+	expect["edge1"] = Edge{
+		keys:     []int{1, 2},
+		operator: MULT,
+	}
+	actual := make(map[string]Edge)
+	node0 := b.AddNode(v)
+	node1 := b.AddNode(v)
+	node2 := b.AddNode(v)
+	actual["edge0"] = b.addEdge(*node0, *node1, ADD)
+	actual["edge1"] = b.addEdge(*node1, *node2, MULT)
+	for i := 0; i < 2; i++ {
+		if actual["edge0"].keys[i] != expect["edge0"].keys[i] {
+			t.Error(errorMsg("unexpected edge0 key", actual["edge0"].keys[i], expect["edge0"].keys[i]))
+		}
+	}
+	if actual["edge0"].operator != expect["edge0"].operator {
+		t.Error(errorMsg("unexpected edge0 operator", actual["edge0"].operator, expect["edge0"].operator))
+	}
+	for i := 0; i < 2; i++ {
+		if actual["edge1"].keys[i] != expect["edge1"].keys[i] {
+			t.Error(errorMsg("unexpected edge1 key", actual["edge1"].keys[i], expect["edge1"].keys[i]))
+		}
+	}
+	if actual["edge1"].operator != expect["edge1"].operator {
+		t.Error(errorMsg("unexpected edge0 operator", actual["edge1"].operator, expect["edge1"].operator))
+	}
+
 }
 
-func TestConstant(t *testing.T) {
+func TestBuilderConstant(t *testing.T) {
+	b := NewBuilder()
+	expect := &Node{
+		value: 7,
+	}
+	actual := b.Constant(7)
+	if actual.value != expect.value {
+		t.Error(errorMsg("unexpected constant value", actual.value, expect.value))
+	}
 
 }
 
-func TestAdd(t *testing.T) {
+func TestBuilderAdd(t *testing.T) {
+	b := NewBuilder()
+	var v uint64
+	node0 := b.AddNode(v)
+	node1 := b.AddNode(v)
+	expect := Edge{
+		keys:     []int{node0.key, node1.key},
+		operator: ADD,
+	}
+	actual := b.Add(*node0, *node1)
+	for i := 0; i < 2; i++ {
+		if actual.parents.keys[i] != expect.keys[i] {
+			t.Error(errorMsg("unexpected parents key in Add", actual.parents.keys[i], expect.keys[i]))
+		}
+	}
+	if actual.parents.operator != expect.operator {
+		t.Error(errorMsg("unexpected parents operator in Add", actual.parents.operator, expect.operator))
+	}
+}
+
+func TestBuilderMult(t *testing.T) {
+	b := NewBuilder()
+	var v uint64
+	node0 := b.AddNode(v)
+	node1 := b.AddNode(v)
+	expect := Edge{
+		keys:     []int{node0.key, node1.key},
+		operator: MULT,
+	}
+	actual := b.Mult(*node0, *node1)
+	for i := 0; i < 2; i++ {
+		if actual.parents.keys[i] != expect.keys[i] {
+			t.Error(errorMsg("unexpected parents key in Add", actual.parents.keys[i], expect.keys[i]))
+		}
+	}
+	if actual.parents.operator != expect.operator {
+		t.Error(errorMsg("unexpected parents operator in Add", actual.parents.operator, expect.operator))
+	}
 
 }
 
-func TestMult(t *testing.T) {
+func TestBuilderAssertEqual(t *testing.T) {
+	b := NewBuilder()
+	var v uint64
+	node0 := b.AddNode(v)
+	node1 := b.AddNode(v)
+	expect := true
+	actual := b.AssertEqual(*node0, *node1)
+	if actual != expect {
+		t.Error(errorMsg("problem with AssertEqual", actual, expect))
+	}
 
 }
 
-func TestAssertEqual(t *testing.T) {
+func TestBuilderFillNodes(t *testing.T) {
+	// f(x) = x^2 + x + 5 would be defined by the following code:
+	builder := NewBuilder()
+	x := builder.Init(6)
+	// evaluates to 47
+	expect := uint64(47)
+	xSquared := builder.Mult(*x, *x)
+	five := builder.Constant(5)
+	xSquaredPlus5 := builder.Add(*xSquared, *five)
+	y := builder.Add(*xSquaredPlus5, *x)
+	builder.FillNodes()
+	actual := y.Get()
+	if actual != expect {
+		t.Error(errorMsg("the graph did not evaluate properly", actual, expect))
+	}
 
 }
 
-func TestFillNodes(t *testing.T) {
+func TestBuilderCheckConstraints(t *testing.T) {
 
 }
 
-func TestCheckConstraints(t *testing.T) {
-
+func TestNodeGet(t *testing.T) {
+	b := NewBuilder()
+	n := b.AddNode(9)
+	expect := uint64(9)
+	actual := n.Get()
+	if actual != expect {
+		t.Error(errorMsg("problem with Get", actual, expect))
+	}
 }
-
